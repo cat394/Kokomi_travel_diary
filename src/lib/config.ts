70eff68016052660e47317d0c7553ec82558487b
Ryo <keyring1231@gmail.com>
2023-09-20 00:32:07 +0900

    ファビコンの追加

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment';
 
 export const title = "Kokomi's travel diary";
 export const description = 'これはKokomiの旅の日記です。';
-export const url = dev ? 'https://localhost:5713/' : 'https://kokomi-travel.app';
+export const url = dev ? 'https://localhost:5713/' : 'https://kokomi-travel-diary.app';
