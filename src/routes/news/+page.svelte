bf22609bf374672a8a8519b83ce6495d457c9f09
Ryo <keyring1231@gmail.com>
2023-09-29 17:00:02 +0900

    お知らせコーナーの追加

---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Contents from "$lib/components/utils/Contents.svelte";
+  import Container from "$lib/components/utils/Container.svelte";
+</script>
+
+<Contents>
+  <Container>
+    
+  </Container>
+</Contents>
