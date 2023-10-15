6a75e33800cc8f8cfd63e5a04da14c5ab013aee7
Ryo <keyring1231@gmail.com>
2023-10-15 23:27:21 +0900

    add: ニュースに新しい項目を追加

--- src/lib/data/announcements.ts
+++ src/lib/data/announcements.ts
@@ -6,7 +6,8 @@ type Announcement = {
 const announcements: Announcement[] = [
 	{ date: '2023/09/19', description: 'サイトを立ち上げた日！' },
 	{ date: '2023/09/21', description: 'クレジットの一覧ページ(最下部にリンクがあるよ)を追加した！' },
-	{ date: '2023/09/29', description: 'Kokomiのミニキャラが出現！' }
+	{ date: '2023/09/29', description: 'Kokomiのミニキャラが出現！' },
+	{ date: '2023/10/14', description: 'ハロウィン仕様＆2回目の投稿！'}
 ];
 
 const sortedAnnouncements = announcements.sort(
